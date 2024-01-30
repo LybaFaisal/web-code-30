4ed6bec1a5a725bbbe4f343617fb4a9babd66973
Lyba <faisallyba06@gmail.com>
2024-01-30 09:07:18 +0500

    Student Info

--- objects.js
+++ objects.js
@@ -1,3 +1,19 @@
+//Basic Example
 let car = { Model: "2020", Brand: "Cultus", Color: "White" };
 
 console.log(`The model of my car is ${car["Model"]}`);
+
+//Student Example
+
+let Student={
+    name: "Lyba",
+    age: 20,
+    grade: "senior",
+    uni: "TUF"
+};
+
+console.log("Student Information:");
+console.log(`The name of student: ${Student.name}`);
+console.log(`The age of student: ${Student.age}`);
+console.log(`The grade of student: ${Student.grade}`);
+console.log(`The uni of student: ${Student.uni}`);
