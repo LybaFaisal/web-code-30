8214c58538b11b95fe50782de2a86795321289c7
Lyba <faisallyba06@gmail.com>
2024-01-30 09:13:31 +0500

    Product Catalog

--- objects.js
+++ objects.js
@@ -17,3 +17,16 @@ console.log(`The name of student: ${Student.name}`);
 console.log(`The age of student: ${Student.age}`);
 console.log(`The grade of student: ${Student.grade}`);
 console.log(`The uni of student: ${Student.uni}`);
+
+//Product Catalog
+
+let laptop={
+    generation: "AMD",
+    price: "High",
+    battery_timing: "Good",
+};
+
+console.log("Laptop Info:");
+console.log(`Generation:  ${laptop.generation}`);
+console.log(`Price:  ${laptop.price}`);
+console.log(`Battery Timing:  ${laptop.battery_timing}`);
